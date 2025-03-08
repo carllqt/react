a104015d9469d81f489c71dca158850d9ba25a76
carllqt <reycarlmedico@gmail.com>
2025-03-08 20:07:16 +0800

    testing

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><div className='p-5 bg-amber-300 rounded-t-xl hover:bg-amber-950 transition-all duration-500'>
+        <h1 className='text-emerald-400'>REACT</h1>
+        <h2>Tailwind</h2>
+        <h3>Tailwind</h3>
+        <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+    </>
+  )
+}
+
+export default App
